bcaee56e1d61041e6528f02dcc6c4bf894339363
Likh-Alex <likhachov991@protonmail.com>
2021-01-11 12:02:18 +0200

    - refactored check for annotation with `isAnnotationPresent` method where suits better - refactored to check `field` by `type` rather than by `name`

--- src/main/java/lib/Injector.java
+++ src/main/java/lib/Injector.java
@@ -1,5 +1,9 @@
 package lib;
 
+import dao.BetDao;
+import dao.PersonDao;
+import dao.implementation.BetDaoImpl;
+import dao.implementation.PersonDaoImpl;
 import exception.NoSuchAnnotationException;
 import factory.Factory;
 import java.lang.reflect.Constructor;
@@ -7,9 +11,8 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 
 public class Injector {
-    public static final String BET_DAO = "betDao";
-    public static final String PERSON_DAO = "personDao";
-    public static final Class<Dao> CLASS_ANNOTATION = Dao.class;
+    public static final Class<Dao> DAO_ANNOTATION = Dao.class;
+    public static final Class<Inject> INJECT_ANNOTATION = Inject.class;
 
     public static Object getInstance(Class clazz) throws NoSuchMethodException,
             IllegalAccessException, InvocationTargetException,
@@ -18,20 +21,18 @@ public class Injector {
         Object instance = constructor.newInstance();
         Field[] declaredFields = clazz.getDeclaredFields();
         for (Field field : declaredFields) {
-            if (field.getAnnotation(Inject.class) != null) {
-                if (field.getName().equals(BET_DAO)
-                        && Factory.getBetDao().getClass()
-                        .getAnnotation(CLASS_ANNOTATION) != null) {
+            if (field.isAnnotationPresent(INJECT_ANNOTATION)) {
+                if (field.getType() == BetDao.class
+                        && BetDaoImpl.class.isAnnotationPresent(DAO_ANNOTATION)) {
                     field.setAccessible(true);
                     field.set(instance, Factory.getBetDao());
-                } else if (field.getName().equals(PERSON_DAO)
-                        && Factory.getPersonDao().getClass()
-                        .getAnnotation(CLASS_ANNOTATION) != null) {
+                } else if (field.getType() == PersonDao.class
+                        && PersonDaoImpl.class.isAnnotationPresent(DAO_ANNOTATION)) {
                     field.setAccessible(true);
                     field.set(instance, Factory.getPersonDao());
                 } else {
                     throw new NoSuchAnnotationException("There is no such annotation: "
-                            + CLASS_ANNOTATION + " in : " + clazz.getName());
+                            + DAO_ANNOTATION + " in : " + clazz.getName());
                 }
             }
         }
